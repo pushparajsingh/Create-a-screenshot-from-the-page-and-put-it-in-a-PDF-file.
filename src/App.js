0be42626ceef52b88f2d695d3bdab822a3182966
Pushparaj Singh <pushparajsingh448@gmail.com>
2023-02-21 17:04:21 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReactToPrint } from "react-to-print";
+import html2canvas from "html2canvas";
+import jsPdf from "jspdf";
+import React, { useRef } from "react";
 
 function App() {
+  const printPDF = () => {
+    const domElement = document.getElementById("raj");
+    html2canvas(domElement, {
+      onclone: (document) => {
+        document.getElementById("print").style.visibility = "hidden";
+      },
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPdf();
+      pdf.addImage(imgData, "JPEG", 10, 10);
+      pdf.save(`${new Date().toISOString()}.pdf`);
+    });
+  };
+
+  // window.onclick = (e) => {
+  //   console.log(e.target); // to get the element
+  //   console.log(e.target.id); // to get the element tag name alone
+  // };
+
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="raj">
+      <h1>Generate PDF</h1>
+      <p>Create a screenshot from the page, and put it in a PDF file.</p>
+      <p style={{ color: "red" }}>
+        *Open this page in new window and press the button.
+      </p>
+      <button id="print" onClick={printPDF}>
+        PRINT
+      </button>
+      <div id="raj">
+        <h2>Ambalika</h2>
+      </div>
+      <button onClick={handlePrint}>Print this out!</button>
+
+      <div ref={componentRef}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+        praesentium sequi est dicta, voluptas laborum temporibus ea
+        reprehenderit quae, amet sapiente at, minima quis. Architecto minus
+        laboriosam ex quasi saepe!
+      </div>
     </div>
   );
 }
